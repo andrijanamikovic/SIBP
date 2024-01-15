60adf72c4edaceb731b6efca62be692a44bb52e8
Andrijana Mikovic <andrijana.mikovic00@gmail.com>
2024-01-15 22:21:02 +0100

    Added biLSTM and CNN

--- test.py
+++ test.py
@@ -10,47 +10,26 @@ sns.set_theme()
 import tensorflow as tf
 from keras import backend as K
 from keras.models import Sequential
-from keras.layers import LSTM
-from keras.layers.core import Dense, Activation, Dropout
+from keras.layers import LSTM, MaxPooling1D
+from keras.layers.core import Dense, Dropout
 from keras.layers import Embedding
 from keras.utils import np_utils, pad_sequences
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
-from keras.layers import SpatialDropout1D
-from keras.preprocessing import sequence, text
+from keras.layers import SpatialDropout1D, Bidirectional, Conv1D
+from keras.preprocessing import text
 from keras.callbacks import EarlyStopping
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 sample = pd.read_csv('Data/sample_submission/sample_submission.csv')
 test = pd.read_csv('Data/test/test.csv')
 train = pd.read_csv('Data/train/train.csv')
 output_file_path = 'test_output.txt'
 
-def multiclass_log_loss(y_true_binarized, y_pred_probabilities, epsilon=1e-20):
-    """
-    Computes the multiclass version oLf Log Loss metric
-    Args:
-        y_true_binarized (array_like, shape (m, n_class)): Matrix of binarized true target classes
-        y_pred_probabilities (array_like, shape (m, n_class)): Matrix of predicted class probabilities
-        eps (scalar): Clipping parameter for predicted class probabilities
-            Class probabilities outside the interval (eps, 1 - eps) are clipped to the nearest endpoint
-    Returns:
-        logloss (scalar): Multiclass log loss obtained from y_true and y_pred (eps-clipped)
-    """
-    # Clipping to avoid undefined quantity log 0
-    y_pred_probabilities = K.clip(y_pred_probabilities, epsilon, 1 - epsilon)
-
-    # Casting sum of the elements of Hadamard product in (3)
-    sum_ = tf.cast(K.sum(y_true_binarized * K.log(y_pred_probabilities)), tf.float64)
-
-    # Computing log loss
-    logloss = (-1 / len(y_true_binarized)) * sum_
-
-    return logloss
-
 def clean_stopwords(text):
     stop_words = nltk.corpus.stopwords.words('english')
     word_tokens = word_tokenize(text)
-    filtered_text = [word for word in word_tokens if word.lower() not in stop_words]
+    filtered_text = [word.lower() for word in word_tokens if word.lower() not in stop_words]
     filtered_text = ' '.join(filtered_text)
     return filtered_text
 
@@ -157,108 +136,239 @@ for word, i in word_index.items():
 
 print(f"Shape of the matrix of word vectors: {word_vectorization_matrix.shape}")
 
+
+
 # LSTM with GloVe embeddings
-model = Sequential()
-model.add(Embedding(len(word_index) + 1,
-                    dim_glove,
-                    weights = [word_vectorization_matrix],
-                    input_length = maxlen_,
-                    trainable = False))
+# model = Sequential()
+# model.add(Embedding(len(word_index) + 1,
+#                     dim_glove,
+#                     weights = [word_vectorization_matrix],
+#                     input_length = maxlen_,
+#                     trainable = False))
+#
+# model.add(SpatialDropout1D(0.3))
+# model.add(LSTM(dim_glove, dropout = 0.3, recurrent_dropout = 0.3))
+#
+# model.add(Dense(512, activation = 'relu'))
+# model.add(Dropout(0.8))
+#
+# model.add(Dense(256, activation = 'relu'))
+# model.add(Dropout(0.8))
+#
+# model.add(Dense(3, activation = 'softmax'))
+#
+# print(model.summary())
+# initial_learning_rate = 0.001
+# model.compile(loss = multiclass_log_loss,
+#               optimizer = tf.keras.optimizers.Adam(learning_rate = initial_learning_rate))
+# # Early stopping callback
+# earlystop = EarlyStopping(monitor = 'val_loss',
+#                           min_delta = 0.001,
+#                           patience = 20,
+#                           verbose = 1,
+#                           mode = 'auto')
+#
+#
+# # Modified linear schedule function
+# def scheduler_modified_exponential(epoch, learning_rate):
+#     if epoch < 40:
+#         return learning_rate
+#     else:
+#         return learning_rate * math.exp(-0.1)
+#
+#
+# learning_rate, epoch, num_epochs, learning_rate_list = initial_learning_rate, 0, 100, []
+# for i in range(num_epochs):
+#     learning_rate = scheduler_modified_exponential(epoch, learning_rate)
+#     learning_rate_list.append(learning_rate)
+#     epoch += 1
+# plt.figure(figsize=(15, 10))
+# plt.plot(learning_rate_list)
+# plt.title("Learning rate vs Epoch", fontsize=14)
+# plt.xlabel("Epoch", fontsize=14)
+# plt.ylabel("Learning rate", fontsize=14)
+# plt.savefig('learningrate_modified_expoential.jpg')
+# plt.show()
+#
+# learning_rate_scheduler = tf.keras.callbacks.LearningRateScheduler(scheduler_modified_exponential)
+# # Model fitting
+# history = model.fit(x_train_pad,
+#                     y = y_train_matrix,
+#                     batch_size = 256,
+#                     epochs = 100,
+#                     verbose = 0,
+#                     validation_data = (x_valid_pad, y_valid_matrix),
+#                     callbacks = [earlystop, learning_rate_scheduler])
+#
+# # Visualization of model loss
+# plt.figure(figsize = (15, 10))
+# plt.title('Model loss (multiclass log loss)', fontsize = 14)
+# sns.lineplot(data = history.history['loss'], label = 'Train')
+# sns.lineplot(data = history.history['val_loss'], label = 'Validation')
+# plt.xlabel('Epoch', fontsize = 14)
+# plt.ylabel('Loss', fontsize = 14)
+# plt.legend()
+# plt.savefig('logloss.jpg')
+# plt.show()
+#
+# y_train_matrix_pred = model.predict(x_train_pad)
+# y_valid_matrix_pred = model.predict(x_valid_pad)
+#
+# # Log loss
+# logloss_train = multiclass_log_loss(y_train_matrix, y_train_matrix_pred).numpy()
+# logloss_valid = multiclass_log_loss(y_valid_matrix, y_valid_matrix_pred).numpy()
+#
+# # Log loss (computed using model.evaluate)
+# # logloss_train = model.evaluate(x_train_pad, y_train_matrix, batch_size = 256, verbose = 0)
+# # logloss_valid = model.evaluate(x_valid_pad, y_valid_matrix, batch_size = 256, verbose = 0)
+#
+# print(f"Training logloss  : {round(logloss_train, 3)}")
+# print(f"Validation logloss: {round(logloss_valid, 3)}")
+#
+# # Converting probability vectors to labels
+# y_train_true = np.array([np.argmax(x) for x in y_train_matrix])
+# y_valid_true = np.array([np.argmax(x) for x in y_valid_matrix])
+# y_train_pred = np.array([np.argmax(x) for x in y_train_matrix_pred])
+# y_valid_pred = np.array([np.argmax(x) for x in y_valid_matrix_pred])
+# # Accuracy
+# match_train = (y_train_true == y_train_pred)
+# match_train = np.array(list(map(lambda x: int(x == True), match_train)))
+# match_valid = (y_valid_true == y_valid_pred)
+# match_valid = np.array(list(map(lambda x: int(x == True), match_valid)))
+#
+# print(f"Training accuracy  : {round(match_train.sum() / len(match_train), 3)}")
+# print(f"Validation accuracy: {round(match_valid.sum() / len(match_valid), 3)}")
 
-model.add(SpatialDropout1D(0.3))
-model.add(LSTM(dim_glove, dropout = 0.3, recurrent_dropout = 0.3))
-
-model.add(Dense(512, activation = 'relu'))
-model.add(Dropout(0.8))
-
-model.add(Dense(256, activation = 'relu'))
-model.add(Dropout(0.8))
-
-model.add(Dense(3, activation = 'softmax'))
-
-print(model.summary())
-initial_learning_rate = 0.001
-model.compile(loss = multiclass_log_loss,
-              optimizer = tf.keras.optimizers.Adam(learning_rate = initial_learning_rate))
-# Early stopping callback
-earlystop = EarlyStopping(monitor = 'val_loss',
-                          min_delta = 0.001,
-                          patience = 20,
-                          verbose = 1,
-                          mode = 'auto')
-
-
-# Modified linear schedule function
-def scheduler_modified_exponential(epoch, learning_rate):
-    if epoch < 40:
-        return learning_rate
+def create_and_train_model(mode, x_train_pad, y_train_matrix, x_valid_pad, y_valid_matrix,
+                            word_index, word_vectorization_matrix, dim_glove, maxlen_):
+    model = Sequential()
+    model.add(Embedding(len(word_index) + 1,
+                        dim_glove,
+                        weights=[word_vectorization_matrix],
+                        input_length=maxlen_,
+                        trainable=False))
+    model.add(SpatialDropout1D(0.3))
+    if mode == "LSTM":
+        model.add(LSTM(dim_glove, dropout=0.3, recurrent_dropout=0.3))
+    elif mode == "BiLSTM":
+        model.add(Bidirectional(LSTM(dim_glove, dropout=0.3, recurrent_dropout=0.3)))
+    elif mode == "CNN":
+        model.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
+        model.add(MaxPooling1D(pool_size=2))
+        model.add(LSTM(dim_glove, dropout=0.3, recurrent_dropout=0.3))
     else:
-        return learning_rate * math.exp(-0.1)
+        return
+
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.8))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.8))
+    model.add(Dense(3, activation='softmax'))
+
+    print(model.summary())
+
+    initial_learning_rate = 0.001
+    model.compile(loss=multiclass_log_loss,
+                  optimizer=tf.keras.optimizers.Adam(learning_rate=initial_learning_rate))
+
+    # Early stopping callback
+    earlystop = EarlyStopping(monitor='val_loss',
+                              min_delta=0.001,
+                              patience=20,
+                              verbose=1,
+                              mode='auto')
+
+    # Modified linear schedule function
+    def scheduler_modified_exponential(epoch, learning_rate):
+        if epoch < 40:
+            return learning_rate
+        else:
+            return learning_rate * math.exp(-0.1)
+
+    learning_rate, epoch, num_epochs, learning_rate_list = initial_learning_rate, 0, 100, []
+    for i in range(num_epochs):
+        learning_rate = scheduler_modified_exponential(epoch, learning_rate)
+        learning_rate_list.append(learning_rate)
+        epoch += 1
+
+    plt.figure(figsize=(15, 10))
+    plt.plot(learning_rate_list)
+    plt.title("Learning rate vs Epoch", fontsize=14)
+    plt.xlabel("Epoch", fontsize=14)
+    plt.ylabel("Learning rate", fontsize=14)
+    plt.savefig(f'learningrate_modified_exponential{mode}.jpg')
+    plt.show()
+
+    learning_rate_scheduler = tf.keras.callbacks.LearningRateScheduler(scheduler_modified_exponential)
+
+    # Model fitting
+    history = model.fit(x_train_pad,
+                        y=y_train_matrix,
+                        batch_size=256,
+                        epochs=100,
+                        verbose=0,
+                        validation_data=(x_valid_pad, y_valid_matrix),
+                        callbacks=[earlystop, learning_rate_scheduler])
+
+    # Visualization of model loss
+    plt.figure(figsize=(15, 10))
+    plt.title('Model loss (multiclass log loss)', fontsize=14)
+    sns.lineplot(data=history.history['loss'], label='Train')
+    sns.lineplot(data=history.history['val_loss'], label='Validation')
+    plt.xlabel('Epoch', fontsize=14)
+    plt.ylabel('Loss', fontsize=14)
+    plt.legend()
+    plt.savefig(f'logloss{mode}.jpg')
+    plt.show()
+    y_train_matrix_pred = model.predict(x_train_pad)
+    y_valid_matrix_pred = model.predict(x_valid_pad)
+
+    # Log loss
+    logloss_train = multiclass_log_loss(y_train_matrix, y_train_matrix_pred).numpy()
+    logloss_valid = multiclass_log_loss(y_valid_matrix, y_valid_matrix_pred).numpy()
+
+    # Log loss (computed using model.evaluate)
+    # logloss_train = model.evaluate(x_train_pad, y_train_matrix, batch_size = 256, verbose = 0)
+    # logloss_valid = model.evaluate(x_valid_pad, y_valid_matrix, batch_size = 256, verbose = 0)
+
+    print(f"Training logloss  : {round(logloss_train, 3)}")
+    print(f"Validation logloss: {round(logloss_valid, 3)}")
+
+    # Converting probability vectors to labels
+    y_train_true = np.array([np.argmax(x) for x in y_train_matrix])
+    y_valid_true = np.array([np.argmax(x) for x in y_valid_matrix])
+    y_train_pred = np.array([np.argmax(x) for x in y_train_matrix_pred])
+    y_valid_pred = np.array([np.argmax(x) for x in y_valid_matrix_pred])
+    # Accuracy
+    accuracy_train = accuracy_score(y_train_true, y_train_pred)
+    accuracy_valid = accuracy_score(y_valid_true, y_valid_pred)
+
+    print(f"Training Accuracy: {accuracy_train:.4f}")
+    print(f"Validation Accuracy: {accuracy_valid:.4f}")
+
+    # Precision, Recall, and F1 Score
+    precision_train = precision_score(y_train_true, y_train_pred, average='weighted')
+    precision_valid = precision_score(y_valid_true, y_valid_pred, average='weighted')
+
+    recall_train = recall_score(y_train_true, y_train_pred, average='weighted')
+    recall_valid = recall_score(y_valid_true, y_valid_pred, average='weighted')
+
+    f1_train = f1_score(y_train_true, y_train_pred, average='weighted')
+    f1_valid = f1_score(y_valid_true, y_valid_pred, average='weighted')
+
+    print(f"Training Precision: {precision_train:.4f}")
+    print(f"Validation Precision: {precision_valid:.4f}")
+
+    print(f"Training Recall: {recall_train:.4f}")
+    print(f"Validation Recall: {recall_valid:.4f}")
+
+    print(f"Training F1 Score: {f1_train:.4f}")
+    print(f"Validation F1 Score: {f1_valid:.4f}")
+
+    return model, history, y_valid_true, y_valid_pred
 
 
-learning_rate, epoch, num_epochs, learning_rate_list = initial_learning_rate, 0, 100, []
-for i in range(num_epochs):
-    learning_rate = scheduler_modified_exponential(epoch, learning_rate)
-    learning_rate_list.append(learning_rate)
-    epoch += 1
-plt.figure(figsize=(15, 10))
-plt.plot(learning_rate_list)
-plt.title("Learning rate vs Epoch", fontsize=14)
-plt.xlabel("Epoch", fontsize=14)
-plt.ylabel("Learning rate", fontsize=14)
-plt.savefig('learningrate_modified_expoential.jpg')
-plt.show()
-
-learning_rate_scheduler = tf.keras.callbacks.LearningRateScheduler(scheduler_modified_exponential)
-# Model fitting
-history = model.fit(x_train_pad,
-                    y = y_train_matrix,
-                    batch_size = 256,
-                    epochs = 100,
-                    verbose = 0,
-                    validation_data = (x_valid_pad, y_valid_matrix),
-                    callbacks = [earlystop, learning_rate_scheduler])
-
-# Visualization of model loss
-plt.figure(figsize = (15, 10))
-plt.title('Model loss (multiclass log loss)', fontsize = 14)
-sns.lineplot(data = history.history['loss'], label = 'Train')
-sns.lineplot(data = history.history['val_loss'], label = 'Validation')
-plt.xlabel('Epoch', fontsize = 14)
-plt.ylabel('Loss', fontsize = 14)
-plt.legend()
-plt.savefig('logloss.jpg')
-plt.show()
-
-y_train_matrix_pred = model.predict(x_train_pad)
-y_valid_matrix_pred = model.predict(x_valid_pad)
-
-# Log loss
-logloss_train = multiclass_log_loss(y_train_matrix, y_train_matrix_pred).numpy()
-logloss_valid = multiclass_log_loss(y_valid_matrix, y_valid_matrix_pred).numpy()
-
-# Log loss (computed using model.evaluate)
-# logloss_train = model.evaluate(x_train_pad, y_train_matrix, batch_size = 256, verbose = 0)
-# logloss_valid = model.evaluate(x_valid_pad, y_valid_matrix, batch_size = 256, verbose = 0)
-
-print(f"Training logloss  : {round(logloss_train, 3)}")
-print(f"Validation logloss: {round(logloss_valid, 3)}")
-
-# Converting probability vectors to labels
-y_train_true = np.array([np.argmax(x) for x in y_train_matrix])
-y_valid_true = np.array([np.argmax(x) for x in y_valid_matrix])
-y_train_pred = np.array([np.argmax(x) for x in y_train_matrix_pred])
-y_valid_pred = np.array([np.argmax(x) for x in y_valid_matrix_pred])
-# Accuracy
-match_train = (y_train_true == y_train_pred)
-match_train = np.array(list(map(lambda x: int(x == True), match_train)))
-match_valid = (y_valid_true == y_valid_pred)
-match_valid = np.array(list(map(lambda x: int(x == True), match_valid)))
-
-print(f"Training accuracy  : {round(match_train.sum() / len(match_train), 3)}")
-print(f"Validation accuracy: {round(match_valid.sum() / len(match_valid), 3)}")
-
-def conf_matrix(y_true, y_pred, n_class, class_names = 'default', figsize = (6.25, 5), font_scale = 1, annot_kws_size = 12):
+def conf_matrix(y_true, y_pred, n_class, class_names, figsize = (6.25, 5), font_scale = 1, annot_kws_size = 12):
     if class_names == 'default':
         class_names = np.arange(n_class)
     tick_marks_y = np.arange(n_class) + 0.5
@@ -277,5 +387,16 @@ def conf_matrix(y_true, y_pred, n_class, class_names = 'default', figsize = (6.2
     plt.savefig('confmat.jpg')
     plt.show()
 
-conf_matrix(y_valid_true, y_valid_pred, n_class = 3, class_names = ['EAP', 'HPL', 'MWS'])
 
+lstm_model, lstm_history, lstm_y_valid_true, lstm_y_valid_pred = create_and_train_model('LSTM', x_train_pad, y_train_matrix, x_valid_pad, y_valid_matrix,
+                                                  word_index, word_vectorization_matrix, dim_glove, maxlen_)
+conf_matrix(lstm_y_valid_true, lstm_y_valid_pred, n_class = 3, class_names = ['EAP', 'HPL', 'MWS'])
+
+bilstm_model, bilstm_history, bilstm_y_valid_true, bulstm_y_valid_pred = create_and_train_model('BiLSTM', x_train_pad, y_train_matrix, x_valid_pad, y_valid_matrix,
+                                                  word_index, word_vectorization_matrix, dim_glove, maxlen_)
+conf_matrix(bilstm_y_valid_true, bulstm_y_valid_pred, n_class = 3, class_names = ['EAP', 'HPL', 'MWS'])
+
+
+cnn_model, cnn_history, cnn_y_valid_true, cnn_y_valid_pred = create_and_train_model('CNN', x_train_pad, y_train_matrix, x_valid_pad, y_valid_matrix,
+                                                  word_index, word_vectorization_matrix, dim_glove, maxlen_)
+conf_matrix(cnn_y_valid_true, cnn_y_valid_pred, n_class = 3, class_names = ['EAP', 'HPL', 'MWS'])
